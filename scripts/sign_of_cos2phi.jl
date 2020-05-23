b6b92db67d9bef95fd86f2226b6365e068fca827
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2020-05-23 19:03:28 +0200

    phi spectra with weights

--- scripts/sign_of_cos2phi.jl
+++ scripts/sign_of_cos2phi.jl
@@ -1,14 +1,12 @@
 using Plots
+using DrWatson
+
 theme(:wong)
 pyplot()
 
-H1_t = randH(1) # J = 1
-S1_t = sample(500; H = H1_t);
-histogram(getproperty.(S1_t, :ϕ), bins=50)
-
 const Nd = 500;
 
-Mcos2ϕ(S) = sum(cos(2ϕ) for ϕ in S)
+Mcos2ϕ(S) = sum(cos(2ϕ) for ϕ in S) / length(S)
 
 @time M_for_J0 = [Mcos2ϕ(getproperty.(sample(Nd; H = randH(0)), :ϕ)) for _ in 1:1000];
 @time M_for_J1 = [Mcos2ϕ(getproperty.(sample(Nd; H = randH(1)), :ϕ)) for _ in 1:1000];
@@ -24,5 +22,33 @@ let bins = range(-100,100,length=50)
     histogram!(M_for_J0, bins=bins, lab="J=0", α=0.8)
     histogram!(M_for_J1, bins=bins, lab="J=1", α=0.8)
     histogram!(M_for_J2, bins=bins, lab="J=2", α=0.8)
+end
+savefig(joinpath("plots", "moment_M2phi_Nev=500.pdf"))
+
+#                                _|            _|          _|
+#  _|      _|      _|    _|_|          _|_|_|  _|_|_|    _|_|_|_|    _|_|_|
+#  _|      _|      _|  _|_|_|_|  _|  _|    _|  _|    _|    _|      _|_|
+#    _|  _|  _|  _|    _|        _|  _|    _|  _|    _|    _|          _|_|
+#      _|      _|        _|_|_|  _|    _|_|_|  _|    _|      _|_|  _|_|_|
+#                                          _|
+#                                      _|_|
+
+function randMcos2ϕ(J, Nev)
+    H = randH(J)
+    S = [randvars() for _ in 1:Nev]
+    Iv = I4μ.(S; H = H)
+    return sum(cos(2ϕ)*i for (ϕ,i) in zip(getproperty.(S, :ϕ), Iv)) / sum(Iv)
+end
+
+@time M_for_J0_w = [randMcos2ϕ(0, Nd) for _ in 1:1000];
+@time M_for_J1_w = [randMcos2ϕ(1, Nd) for _ in 1:1000];
+@time M_for_J2_w = [randMcos2ϕ(2, Nd) for _ in 1:1000];
+
+let bins = range(-100,100,length=50)
+    plot(title="Odd and even spins",
+        xlab="M2 moment")
+    histogram!(M_for_J0_w * 500, bins=bins, lab="J=0", α=0.8)
+    histogram!(M_for_J1_w * 500, bins=bins, lab="J=1", α=0.8)
+    histogram!(M_for_J2_w * 500, bins=bins, lab="J=2", α=0.8)
 end
 savefig(joinpath("plots", "moment_M2phi_Nev=500.pdf"))
