4d5848a61eb4218ad61f545917313cc59438bfd9
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2020-06-03 15:50:38 +0200

    moved the particle-2 phase to angular functions

--- src/angular_functions.jl
+++ src/angular_functions.jl
@@ -9,8 +9,8 @@ function I4μ(vars; H=error("helicity coupling"))
     @unpack cosθ1,cosθ2,ϕ = vars
     return real(sum(
         (λ1-λ2 == λ1′-λ2′ ? 1 : 0) *
-            wignerd(1,λ1 ,ξ1,cosθ1) * wignerd(1,λ2 ,ξ2,cosθ2) *
-            wignerd(1,λ1′,ξ1,cosθ1) * wignerd(1,λ2′,ξ2,cosθ2) *
+            wignerd(1,λ1 ,ξ1,cosθ1) * wignerd(1,λ2 ,ξ2,cosθ2) * (isodd(1-λ2 ) ? -1 : 1) *
+            wignerd(1,λ1′,ξ1,cosθ1) * wignerd(1,λ2′,ξ2,cosθ2) * (isodd(1-λ2′) ? -1 : 1) *
             cis((λ1-λ1′)*ϕ) *
                  H[ λ1+2,λ2+2] *
             conj(H[λ1′+2,λ2′+2])
@@ -23,8 +23,8 @@ function I4K(vars; H=error("helicity coupling"))
     @unpack cosθ1,cosθ2,ϕ = vars
     return real(sum(
         (λ1-λ2 == λ1′-λ2′ ? 1 : 0) *
-            wignerd(1,λ1 ,0,cosθ1) * wignerd(1,λ2 ,0,cosθ2) *
-            wignerd(1,λ1′,0,cosθ1) * wignerd(1,λ2′,0,cosθ2) *
+            wignerd(1,λ1 ,0,cosθ1) * wignerd(1,λ2 ,0,cosθ2) * (isodd(1-λ2 ) ? -1 : 1) *
+            wignerd(1,λ1′,0,cosθ1) * wignerd(1,λ2′,0,cosθ2) * (isodd(1-λ2′) ? -1 : 1) *
             cis((λ1-λ1′)*ϕ1) *
                 H[ λ1+2, λ2+2] *
            conj(H[λ1′+2,λ2′+2])
@@ -32,14 +32,13 @@ function I4K(vars; H=error("helicity coupling"))
                 λ1′ in -1:1, λ2′ in -1:1))
 end
 
-I4μ_LS(vars; LS, J) = I4μ(vars; H=[Hλλ(λ1, λ2;  LS=LS, J=J) for λ1 in -1:1, λ2  in -1:1])
-I4K_LS(vars; LS, J) = I4K(vars; H=[Hλλ(λ1, λ2;  LS=LS, J=J) for λ1 in -1:1, λ2  in -1:1])
-
+# I4μ_LS(vars; LS, J) = I4μ(vars; H=[Hλλ(λ1, λ2;  LS=LS, J=J) for λ1 in -1:1, λ2  in -1:1])
+# I4K_LS(vars; LS, J) = I4K(vars; H=[Hλλ(λ1, λ2;  LS=LS, J=J) for λ1 in -1:1, λ2  in -1:1])
 #
 function I4μ_intϕ(vars; H=error("helicity coupling"))
     @unpack cosθ1,cosθ2 = vars
     return real(sum(
-            wignerd(1,λ1,ξ1,cosθ1) * wignerd(1,λ2 ,ξ2,cosθ2) *
+            wignerd(1,λ1,ξ1,cosθ1) * wignerd(1,λ2,ξ2,cosθ2) *
             wignerd(1,λ1,ξ1,cosθ1) * wignerd(1,λ2,ξ2,cosθ2) *
                 abs2(H[ λ1+2, λ2+2])
             for ξ1 in [-1,1], ξ2 in [-1,1],
