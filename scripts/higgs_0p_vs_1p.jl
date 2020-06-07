18abfd0ec3eae9632e2eb4bdaad6268e3a2e6aa3
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2020-06-07 22:20:42 +0200

    found correct file for hyphothsis testing

--- scripts/higgs_0p_vs_1p.jl
+++ scripts/higgs_0p_vs_1p.jl
@@ -22,8 +22,8 @@ let bins=range(350,500,length=100)
 end
 savefig(plotsdir("llh_testing_higgs.pdf"))
 
-_, fr_1p = get_fit_results(datadir("simulations", "groups_cross_testing", "results_J1_Nev500_Natt5_Nsamples501.jld2"));
-let bins=range(380,450,length=100)
+_, fr_1p = get_fit_results(datadir("simulations", "groups_cross_testing", "results_J3_Nev500_Natt5_Nsamples501.jld2"));
+let bins=range(370,420,length=100)
     plot(size=(500,350))
     stephist!(map(v->v[1].LLH, fr_1p), bins=bins, lab="group-I")
     stephist!(map(v->v[2].LLH, fr_1p), bins=bins, lab="group-II")
@@ -31,9 +31,9 @@ let bins=range(380,450,length=100)
     stephist!(map(v->v[4].LLH, fr_1p), bins=bins, lab="group-IV")
 end
 
-let bins=range(-70,70,length=50)
-    plot(size=(500,350), xlab="TS(0+ / 1-)", ylab="# sample entries")
+let bins=range(-30,70,length=50)
+    plot(size=(500,350), xlab="TS(0+/1-)", ylab="# sample entries")
     stephist!(-map(v->v[1].LLH - v[3].LLH, fr_0p), bins=bins, lab="0+ generated", seriescolor=1)
-    stephist!(map(v->v[1].LLH - v[3].LLH, fr_1p), bins=bins, lab="1- generated", seriescolor=3)
+    stephist!(-map(v->v[1].LLH - v[3].LLH, fr_1p), bins=bins, lab="1- generated", seriescolor=3)
 end
 savefig(plotsdir("TS_0p_vs_1m.pdf"))
