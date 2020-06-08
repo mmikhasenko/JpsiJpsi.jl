2a00ccc6413daadc643c6429275e0ad0fda2b5e2
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2020-06-08 20:11:59 +0200

    Added plots, polished text

--- scripts/higgs_phi.jl
+++ scripts/higgs_phi.jl
@@ -4,6 +4,31 @@ using DrWatson
 using JpsiJpsi
 using Parameters: @unpack
 using LinearAlgebra
+using Plots
+theme(:wong)
+#
+@recipe function f(::Type{Val{:errorhist}}, x, y, z)
+    h = Plots._make_hist((y,), plotattributes[:bins], normed = plotattributes[:normalize], weights = plotattributes[:weights])
+    edge = collect(h.edges[1])
+    centers = Plots._bin_centers(edge)
+    dx = Plots.diff(h.edges[1])/2
+    x := centers
+    y := h.weights
+    seriestype := :scatter
+    @series begin
+        seriestype := :xerror
+        xerror --> dx
+        yerror --> sqrt.(h.weights)
+        (centers, h.weights)
+    end
+    @series begin
+        seriestype := :yerror
+        yerror --> sqrt.(h.weights)
+        (centers, h.weights)
+    end
+    ()
+end
+@shorthands errorhist
 #
 
 fs = [(f=ϕ->(1+cos(2*ϕ)/6)/2π, lab="\"0⁺\""),
@@ -12,10 +37,10 @@ fs = [(f=ϕ->(1+cos(2*ϕ)/6)/2π, lab="\"0⁺\""),
     ];
 S1_t = sample(1000; H = Diagonal(fill(1/sqrt(3),3)))
 
-let
+let Nbins=15
     Nev = length(S1_t)
-    plot(size=(500,350), xlab="ϕ", ylab="# events", leg=:bottomright, xlim=(-π, π))
-    [plot!(ϕ->f(ϕ) * (Nev/10*(2π)), -π, π, lab=lab) for (f,lab) in fs]
-    stephist!(getproperty.(S1_t, :ϕ), bins=range(-π,π, length=11), seriescolor=:black, lab="data")
+    plot(size=(500,350), xlab="ϕ", ylab="# events", xlim=(-π, π))
+    [plot!(ϕ->f(ϕ) * (Nev/Nbins*(2π)), -π, π, lab=lab) for (f,lab) in fs]
+    errorhist!(getproperty.(S1_t, :ϕ), bins=range(-π,π, length=Nbins+1), seriescolor=:black, lab="data")
 end
 savefig(plotsdir("phi_higgs.pdf"))
--- scripts/sign_of_cos2phi.jl
+++ scripts/sign_of_cos2phi.jl
@@ -1,27 +1,26 @@
-using Plots
 using DrWatson
+@quickactivate "JpsiJpsi"
 
+using LinearAlgebra
+
+using Plots
 theme(:wong)
-pyplot()
+# pyplot()
 
-const Nd = 500;
+const Nd = 1000;
 
-Mcos2ϕ(S) = sum(cos(2ϕ) for ϕ in S) / length(S)
+Mcos2ϕ(S) = sum(cos.(2 .* S)) / length(S)
 
-@time M_for_J0 = [Mcos2ϕ(getproperty.(sample(Nd; H = randH(0)), :ϕ)) for _ in 1:1000];
-@time M_for_J1 = [Mcos2ϕ(getproperty.(sample(Nd; H = randH(1)), :ϕ)) for _ in 1:1000];
-@time M_for_J2 = [Mcos2ϕ(getproperty.(sample(Nd; H = randH(2)), :ϕ)) for _ in 1:1000];
+S1_t = [Mcos2ϕ(getproperty.(sample(Nd; H = Diagonal(fill(1/sqrt(3),3))), :ϕ)) for _ in 1:100]
+S2_t = [Mcos2ϕ(getproperty.(sample(Nd; H = Diagonal([1/√2, 0, -1/√2])), :ϕ)) for _ in 1:100]
+S3_t = [Mcos2ϕ(getproperty.(sample(Nd; H = ngHs[3][1]), :ϕ)) for _ in 1:100]
 
-histogram(M_for_J0, bins=100)
-histogram(M_for_J1, bins=100)
-histogram(M_for_J2, bins=100)
-
-let bins = range(-100,100,length=50)
+let bins = range(-0.2,0.2,length=50)
     plot(title="Odd and even spins",
         xlab="M2 moment")
-    histogram!(M_for_J0, bins=bins, lab="J=0", α=0.8)
-    histogram!(M_for_J1, bins=bins, lab="J=1", α=0.8)
-    histogram!(M_for_J2, bins=bins, lab="J=2", α=0.8)
+    histogram!(S1_t, bins=bins, lab="group-I", α=0.8)
+    histogram!(S2_t, bins=bins, lab="group-II", α=0.8)
+    histogram!(S3_t, bins=bins, lab="group-III", α=0.8)
 end
 savefig(joinpath("plots", "moment_M2phi_Nev=500.pdf"))
 
@@ -33,36 +32,36 @@ savefig(joinpath("plots", "moment_M2phi_Nev=500.pdf"))
 #                                          _|
 #                                      _|_|
 
-function randMcos2ϕ(J, Nev)
-    H = randH(J)
+function randMcos2ϕ(ng, Nev)
+    H = randH(ng)
     S = [randvars() for _ in 1:Nev]
     Iv = I4μ.(S; H = H)
     return sum(cos(2ϕ)*i for (ϕ,i) in zip(getproperty.(S, :ϕ), Iv)) / sum(Iv)
 end
 
-@time M_for_J0_w = [randMcos2ϕ(0, Nd) for _ in 1:1000];
-@time M_for_J1_w = [randMcos2ϕ(1, Nd) for _ in 1:1000];
-@time M_for_J2_w = [randMcos2ϕ(2, Nd) for _ in 1:1000];
+@time M_for_ng1_w = [randMcos2ϕ(1, Nd) for _ in 1:1000];
+@time M_for_ng2_w = [randMcos2ϕ(2, Nd) for _ in 1:1000];
+@time M_for_ng3_w = [randMcos2ϕ(3, Nd) for _ in 1:1000];
 
 let bins = range(-0.2,0.2,length=50)
     plot(title="Odd and even spins",
         xlab="M2 moment")
-    histogram!(M_for_J0_w, bins=bins, lab="J=0", α=0.8)
-    histogram!(M_for_J1_w, bins=bins, lab="J=1", α=0.8)
-    histogram!(M_for_J2_w, bins=bins, lab="J=2", α=0.8)
+    histogram!(M_for_ng1_w, bins=bins, lab="group-I", α=0.8)
+    histogram!(M_for_ng2_w, bins=bins, lab="group-II", α=0.8)
+    histogram!(M_for_ng3_w, bins=bins, lab="group-III", α=0.8)
 end
 savefig(joinpath("plots", "moment_M2phi_Nev=$(Nd).pdf"))
 
 const Nd′ = 1500;
-@time M_for_J0_w′ = [randMcos2ϕ(0, Nd′) for _ in 1:1000];
-@time M_for_J1_w′ = [randMcos2ϕ(1, Nd′) for _ in 1:1000];
-@time M_for_J2_w′ = [randMcos2ϕ(2, Nd′) for _ in 1:1000];
+@time M_for_ng1_w′ = [randMcos2ϕ(0, Nd′) for _ in 1:1000];
+@time M_for_ng2_w′ = [randMcos2ϕ(1, Nd′) for _ in 1:1000];
+@time M_for_ng3_w′ = [randMcos2ϕ(2, Nd′) for _ in 1:1000];
 
 let bins = range(-0.2,0.2,length=50)
     plot(title="Odd and even spins",
         xlab="M2 moment")
-    histogram!(M_for_J0_w′, bins=bins, lab="J=0", α=0.8)
-    histogram!(M_for_J1_w′, bins=bins, lab="J=1", α=0.8)
-    histogram!(M_for_J2_w′, bins=bins, lab="J=2", α=0.8)
+    histogram!(M_for_ng1_w′, bins=bins, lab="group-I", α=0.8)
+    histogram!(M_for_ng2_w′, bins=bins, lab="group-II", α=0.8)
+    histogram!(M_for_ng3_w′, bins=bins, lab="group-III", α=0.8)
 end
 savefig(joinpath("plots", "moment_M2phi_Nev=$(Nd′).pdf"))
